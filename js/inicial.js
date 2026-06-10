d779b712d393f07c3e0d4fd00e54b6e456a2536d
Igor jose <lmelo7262@gmail.com>
2026-06-10 19:58:39 -0300

    ajustes de cores e animação

--- js/inicial.js
+++ js/inicial.js
@@ -11,3 +11,22 @@ function(){
         document.getElementById("acerto").style.color="green";
     }
 }
+function clicarbutton()
+{
+    // Function implementation
+
+    let name= "igor jose";
+    let idade= 20;
+    let curso= "analise e desenvolvimento de sistemas";
+
+    if(name=="igor jose" && idade==20 && curso=="analise e desenvolvimento de sistemas"){
+      let entrou= esta correto;
+      document.getElementById("entrou").textContent=entrou;
+      document.getElementById("entrou").style.color="green";
+    } else if(name==" " && idade==" " && curso==" "){
+        let preencha= preencha os campos;
+        document.getElementById("preencha").textContent=preencha;
+        document.getElementById("preencha").style.color="red";
+    }
+}  
+
